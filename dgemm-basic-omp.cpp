6a30dec4911a8e3a167323f0008e5dff41a0a148
shubhe25p <shubhpachchigar@gmail.com>
2023-11-29 11:45:52 -0800

    init single node

---
+++ dgemm-basic-omp.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <omp.h>
+
+const char *dgemm_desc = "Basic implementation, OpenMP-enabled, three-loop dgemm.";
+
+/*
+ * This routine performs a dgemm operation
+ *  C := C + A * B
+ * where A, B, and C are n-by-n matrices stored in column-major format.
+ * On exit, A and B maintain their input values.
+ */
+void square_dgemm(int n, double *A, double *B, double *C)
+{
+// insert your code here: implementation of basic matrix multiply with OpenMP parallelism enabled
+
+// be sure to include LIKWID_MARKER_START(MY_MARKER_REGION_NAME) inside the block of parallel code,
+// but before your matrix multiply code, and then include LIKWID_MARKER_STOP(MY_MARKER_REGION_NAME)
+// after the matrix multiply code but before the end of the parallel code block.
+#pragma omp parallel 
+{
+   #pragma omp for
+   for (int i = 0; i < n; i++)
+   {
+      for (int j = 0; j < n; j++)
+      {
+         double dot = 0.0;
+         for (int k = 0; k < n; k++)
+         {
+            dot += A[j + k * n] * B[i * n + k];
+         }
+         C[i * n + j] += dot;
+      }
+   }
+}
+}
